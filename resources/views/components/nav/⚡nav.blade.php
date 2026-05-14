c9180cf8e5c57355ee2d6ee6b49225d8710b3209
JeanRoyen <jean.royen@student.hepl.be>
2026-05-14 12:05:47 +0200

    Fortify login & logout

--- resources/views/components/nav/⚡nav.blade.php
+++ resources/views/components/nav/⚡nav.blade.php
@@ -16,7 +16,16 @@ new class extends Component
             <livewire:nav.nav-item name="Order" route="/order"/>
             <livewire:nav.nav-item name="Preparation" route="/preparation"/>
             <livewire:nav.nav-item name="Delivery" route="/delivery"/>
-            <livewire:nav.nav-item name="historic" route="/historic"/>
+            <livewire:nav.nav-item name="History" route="/historic"/>
+            @auth
+                <form action="{{ route('logout') }}" method="post">
+                    <button type="submit">Se déconnecter</button>
+                </form>
+            @endauth
+            @guest
+                <a href="{{ route('login.store') }}">Se connecter</a>
+            @endguest
+
         </nav>
     </header>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::middleware(['auth'])->group(function () {
-    Route::livewire('/dashboard', 'pages::dashboard.index')->name('dashboard.index');
+    Route::livewire('/', 'pages::dashboard.index')->name('dashboard.index');
     Route::livewire('/customer', 'pages::customer.index')->name('customer.index');
     Route::livewire('/order', 'pages::order.index')->name('order.index');
     Route::livewire('/preparation', 'pages::preparation.index')->name('preparation.index');
