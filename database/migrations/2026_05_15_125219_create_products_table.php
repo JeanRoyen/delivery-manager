1354714a0584cf8041a2f83b46146b02552d29a8
JeanRoyen <jean.royen@student.hepl.be>
2026-05-18 14:46:12 +0200

    Update price integer -> bigInteger

--- database/migrations/2026_05_15_125219_create_products_table.php
+++ database/migrations/2026_05_15_125219_create_products_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
             $table->id();
             $table->string('name');
             $table->text('description');
-            $table->integer('price');
+            $table->bigInteger('price');
             $table->timestamps();
             $table->softDeletes();
         });
