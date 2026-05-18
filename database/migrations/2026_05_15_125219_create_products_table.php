e5aa6d92f7367367e333f98d3afc941aaf827f45
JeanRoyen <jean.royen@student.hepl.be>
2026-05-18 17:15:42 +0200

    Update migration products: description -> nullable

--- database/migrations/2026_05_15_125219_create_products_table.php
+++ database/migrations/2026_05_15_125219_create_products_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->bigInteger('price');
             $table->timestamps();
             $table->softDeletes();
