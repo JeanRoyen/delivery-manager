200e10d10363cc523a0132d41862a74fa5717db6
JeanRoyen <jean.royen@student.hepl.be>
2026-05-18 13:20:49 +0200

    Crete product index route

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::middleware(['auth', 'language'])->group(function () {
     Route::livewire('/', 'pages::dashboard.index')->name('dashboard.index');
     Route::livewire('/customer', 'pages::customer.index')->name('customer.index');
     Route::livewire('/customer/create', 'pages::customer.create')->name('customer.create');
+    Route::livewire('/product', 'pages::product.index')->name('product.index');
     Route::livewire('/order', 'pages::order.index')->name('order.index');
     Route::livewire('/preparation', 'pages::preparation.index')->name('preparation.index');
     Route::livewire('/delivery', 'pages::delivery.index')->name('delivery.index');
