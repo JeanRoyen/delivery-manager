b012dbd3a3c19be3d089c3f0f1ee5346affe4a51
JeanRoyen <jean.royen@student.hepl.be>
2026-05-19 18:13:02 +0200

    add type to input in login

--- resources/views/components/auth/form.blade.php
+++ resources/views/components/auth/form.blade.php
@@ -1,6 +1,6 @@
 <form action="{{ route('login.store') }}" method="post" class="space-y-4">
     @csrf
-    <flux:input wire:model="email" label="{{ __('auth_form.email') }}" />
-    <flux:input wire:model="password" label="{{ __('auth_form.password') }}" viewable/>
+    <flux:input type="text" wire:model="email" label="{{ __('auth_form.email') }}" />
+    <flux:input type="password" wire:model="password" label="{{ __('auth_form.password') }}" viewable="true"/>
     <flux:button variant="primary" class="w-full mt-4" type="submit">{{ __('auth_form.login') }}</flux:button>
 </form>
