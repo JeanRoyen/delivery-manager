83319b57b385296bbe63e70a45f5fa69cabcc185
JeanRoyen <jean.royen@student.hepl.be>
2026-05-19 16:56:04 +0200

    Add style to login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,29 +12,53 @@
     @fluxAppearance
 
 </head>
-<body class="bg-white dark:bg-zinc-800 flex ">
-<div>
-    <h2>Delivery Manager</h2>
+<body class="bg-white dark:bg-zinc-800 flex min-h-screen">
+<body class="bg-white dark:bg-zinc-800 flex min-h-screen">
+<div class="w-full lg:w-[40%] flex flex-col justify-center px-6 lg:px-16">
+    <h2 class="text-3xl font-bold mb-8 text-center">
+        Delivery Manager
+    </h2>
+
     <flux:card class="space-y-6">
         <div>
-            <flux:heading size="xl">{{ __('auth_form.login_title') }}</flux:heading>
+            <flux:heading size="xl">
+                {{ __('auth_form.login_title') }}
+            </flux:heading>
         </div>
+
         <div>
-            <x-auth.form/>
+            <x-auth.form />
         </div>
     </flux:card>
-    <flux:dropdown>
-        <flux:button icon:trailing="chevron-down">{{ language()->getName() }}</flux:button>
-        <flux:navmenu>
-            @foreach (language()->allowed() as $code => $name)
-                <flux:navmenu.item href="{{ language()->back($code) }}">{{ $name }}</flux:navmenu.item>
-            @endforeach
-        </flux:navmenu>
-    </flux:dropdown>
+
+    <div class="mt-6 flex justify-center lg:justify-start">
+        <flux:dropdown>
+            <flux:button icon:trailing="chevron-down">
+                {{ language()->getName() }}
+            </flux:button>
+
+            <flux:navmenu>
+                @foreach (language()->allowed() as $code => $name)
+                    <flux:navmenu.item href="{{ language()->back($code) }}">
+                        {{ $name }}
+                    </flux:navmenu.item>
+                @endforeach
+            </flux:navmenu>
+        </flux:dropdown>
+    </div>
 </div>
-<div>
-    <img src="{{ asset('img/login_image.webp') }}" alt="{{ __('auth_form.delivery_driver_alt') }}">
+
+<div class="hidden lg:block lg:w-[60%] h-screen">
+    <img
+        src="{{ asset('img/login_image.webp') }}"
+        alt="{{ __('auth_form.delivery_driver_alt') }}"
+        class="w-full h-full object-cover"
+    >
 </div>
+
+@livewireScripts
+@fluxScripts
+</body>
 
 @livewireScripts
 @fluxScripts
