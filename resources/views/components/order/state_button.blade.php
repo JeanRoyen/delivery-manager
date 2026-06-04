9636d000371e665add5c0519da015508fe87cf2d
JeanRoyen <jean.royen@student.hepl.be>
2026-06-04 14:22:11 +0200

    Change color by state to indigo

--- resources/views/components/order/state_button.blade.php
+++ resources/views/components/order/state_button.blade.php
@@ -1,33 +1,35 @@
+@php use App\States\Order\Delivered;use App\States\Order\Delivering;use App\States\Order\Preparing; @endphp
 @props([
     'order'
 ])
 
-@if($order->state->canTransitionTo(\App\States\Order\Preparing::class))
+@if($order->state->canTransitionTo(Preparing::class))
 
     <flux:button
         variant="primary"
-        color="blue"
+        color="indigo"
         icon="play"
         wire:click="updateState('preparing', 'preparing.index')"
     >
         {{ __('order_show.order.start_preparation') }}
     </flux:button>
 
-@elseif($order->state->canTransitionTo(\App\States\Order\Delivering::class))
+@elseif($order->state->canTransitionTo(Delivering::class))
 
     <flux:button
         variant="primary"
-        color="lime"
+        color="indigo"
         icon="truck"
         wire:click="updateState('delivering', 'delivering.index')"
     >
         {{ __('order_show.order.start_delivery') }}
     </flux:button>
 
-@elseif($order->state->canTransitionTo(\App\States\Order\Delivered::class))
+@elseif($order->state->canTransitionTo(Delivered::class))
 
     <flux:button
         variant="primary"
+        color="indigo"
         icon="check"
         wire:click="updateState('delivered', 'delivered.index')"
     >
