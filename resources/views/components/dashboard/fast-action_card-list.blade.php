f7d4507a847c02599ac8348ac265a33f5dae26f4
JeanRoyen <jean.royen@student.hepl.be>
2026-08-17 16:06:02 +0200

    Style | Update action cards

--- resources/views/components/dashboard/fast-action_card-list.blade.php
+++ resources/views/components/dashboard/fast-action_card-list.blade.php
@@ -3,27 +3,33 @@
         title="{{ __('fast-action_cards.new_customer_title') }}"
         button="{{ __('fast-action_cards.create_customer_button') }}"
         :href="route('customer.create')"
+        icon-class="bg-violet-100 text-violet-700 dark:bg-violet-900/40 dark:text-violet-300"
+        button-color="violet"
     >
         <x-slot:icon>
-            <flux:icon.user class="size-10 text-zinc-700" />
+            <flux:icon.user class="size-8" />
         </x-slot:icon>
     </x-dashboard.fast-action_card>
     <x-dashboard.fast-action_card
         title="{{ __('fast-action_cards.new_product_title') }}"
         button="{{ __('fast-action_cards.create_product_button') }}"
         :href="route('product.create')"
+        icon-class="bg-sky-100 text-sky-700 dark:bg-sky-900/40 dark:text-sky-300"
+        button-color="sky"
     >
         <x-slot:icon>
-            <flux:icon.list-bullet class="size-10 text-zinc-700" />
+            <flux:icon.list-bullet class="size-8" />
         </x-slot:icon>
     </x-dashboard.fast-action_card>
     <x-dashboard.fast-action_card
         title="{{ __('fast-action_cards.new_order_title') }}"
         button="{{ __('fast-action_cards.create_order_button') }}"
         :href="route('orders.create')"
+        icon-class="bg-emerald-100 text-emerald-700 dark:bg-emerald-900/40 dark:text-emerald-300"
+        button-color="emerald"
     >
         <x-slot:icon>
-            <flux:icon.cube class="size-10 text-zinc-700" />
+            <flux:icon.cube class="size-8" />
         </x-slot:icon>
     </x-dashboard.fast-action_card>
 
--- resources/views/components/dashboard/fast-action_card.blade.php
+++ resources/views/components/dashboard/fast-action_card.blade.php
@@ -2,21 +2,26 @@
     'title',
     'button',
     'href',
+    'iconClass' => 'bg-zinc-100 text-zinc-700 dark:bg-zinc-800 dark:text-zinc-200',
+    'buttonColor' => 'green',
 ])
 
-<flux:card class="flex items-center gap-5 p-6">
-    <div class="shrink-0">
-        {{ $icon }}
+<flux:card class="group flex h-full flex-col p-6 transition duration-200 hover:-translate-y-1 hover:border-zinc-300 hover:shadow-lg dark:hover:border-zinc-600">
+    <div class="mb-6">
+        <div class="inline-flex rounded-2xl p-3 transition duration-200 group-hover:scale-105 {{ $iconClass }}">
+            {{ $icon }}
+        </div>
     </div>
 
-    <div class="space-y-3">
-        <flux:heading size="lg">
+    <div class="flex flex-1 flex-col items-start gap-5">
+        <flux:heading size="lg" class="text-balance">
             {{ $title }}
         </flux:heading>
 
         <flux:button
+            class="mt-auto w-full justify-center py-2.5"
             variant="primary"
-            color="green"
+            :color="$buttonColor"
             wire:navigate
             href="{{ $href }}"
         >
